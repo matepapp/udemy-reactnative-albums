c594edc2956ac69c59eadfe705fc720f2652e66b
Mate Papp <matepapp@icloud.com>
2017-07-05 15:02:51 +0200

    Lesson 015 - Get content on the screen end.

--- index.ios.js
+++ index.ios.js
@@ -1,1 +1,10 @@
 // index.ios.js - place code in here for iOS
+
+// Import a library to help create a Component
+import React from 'react';
+import ReactNative from 'react-native';
+
+// Create a Component
+
+
+// Render it to the device
