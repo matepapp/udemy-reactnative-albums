efd8943a24898f91e91e8d3dcf16e355292aa90f
Mate Papp <matepapp@icloud.com>
2017-07-06 09:43:15 +0200

    Lesson 026 - Header styling.

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,4 @@
+// @flow
 // Import libraries for making a component
 import React from 'react';
 import { Text, View } from 'react-native';
@@ -16,6 +17,15 @@ const Header = () => {
 const styles = {
   viewStyle: {
     backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative',
   },
   textStyle: {
     fontSize: 20,
