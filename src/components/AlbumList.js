ad9f341d626538037394c0a452024d9033616fd4
Mate Papp <matepapp@icloud.com>
2017-07-06 11:01:07 +0200

    Lesson 031 - Debugging basic.

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AlbumList extends Component {
+  componentWillMount() {
+  }
+
   render() {
     return (
       <View>
