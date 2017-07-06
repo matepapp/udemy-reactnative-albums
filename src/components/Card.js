feb761efddf20cbbd520d3e856de147ae7797c2a
Mate Papp <matepapp@icloud.com>
2017-07-06 12:09:06 +0200

    Lesson 039 - Passing components as props.

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,10 +2,10 @@
 import React from 'react';
 import { View } from 'react-native';
 
-const Card = () => {
+const Card = (props) => {
   return (
     <View style={styles.containerStyle}>
-      
+      {props.children}
     </View>
   );
 };
